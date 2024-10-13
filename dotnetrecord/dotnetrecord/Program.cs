57b4a747fbc6898c18c940977fdd37b7365564a0
unknown <edersongessele@gmail.com>
2024-10-13 12:19:31 -0300

    first commit

---
+++ dotnetrecord/dotnetrecord/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+var usuario1 = new Usuario("teste", "teste@gmail.com");
+var usuario2 = new Usuario("teste", "teste@gmail.com");
+
+Console.WriteLine($"Usuario1 => {usuario1}");
+Console.WriteLine($"Usuario2 => {usuario2}");
+Console.WriteLine($"Usuario 1 é igual ao Usurio 2 => {usuario1 == usuario2}");
+Console.WriteLine($"Sao iguais => {usuario1.Equals(usuario2)}");
+
+var usuario3 = usuario1 with { nome = "teste2" };
+
+Console.WriteLine($"Usuario3 => {usuario3}");
+
+record Usuario(string nome, string email);
